44da35582f9625c337fdf23ac743d356ad1ef7fb
Francisco Souza <f@souza.cc>
2011-11-06 21:43:11 -0200

    Habilitando app markup (para conseguir usar markdown)

--- devincachu/settings.py
+++ devincachu/settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.markup',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
