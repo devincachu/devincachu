67c783135c5455206d0503aef7d6ea81bfb980ca
Francisco Souza <f@souza.cc>
2011-11-06 04:06:33 -0200

    Usando TemplateView para renderizar base.html no index

--- devincachu/urls.py
+++ devincachu/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import include, patterns, url
+from django.views.generic import base
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', base.TemplateView.as_view(template_name='base.html')),
 )
