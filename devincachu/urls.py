6a5f2dd7715d71815c61187623e4841e34d69889
Francisco Souza <f@souza.cc>
2011-11-06 21:43:27 -0200

    Página inicial com listagem de destaques

--- devincachu/urls.py
+++ devincachu/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import include, patterns, url
-from django.views.generic import base
+from django.views.generic import list
 
 from django.contrib import admin
 admin.autodiscover()
 
+from destaques import models
+
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', base.TemplateView.as_view(template_name='base.html')),
+    url(r'^$', list.ListView.as_view(template_name='index.html', queryset=models.Destaque.objects.select_related().order_by('-data')[:10], context_object_name='destaques'), name='index'),
 )
