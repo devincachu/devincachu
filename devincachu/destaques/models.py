59f40813eebc3b02e1c1ad8874e1ad58736cf777
Francisco Souza <f@souza.cc>
2011-11-10 12:55:17 -0200

    Fazendo purge da home ao salvar e apagar destaque

--- devincachu/destaques/models.py
+++ devincachu/destaques/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import roan
 
 from django.contrib.auth import models as auth_models
 from django.db import models
@@ -9,3 +10,6 @@ class Destaque(models.Model):
     conteudo = models.CharField(max_length=500)
     autor = models.ForeignKey(auth_models.User)
     data = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+roan.purge("/").on_save(Destaque)
+roan.purge("/").on_delete(Destaque)
