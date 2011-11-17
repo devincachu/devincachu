aa8eb9174a6e8b74564ab20062ca5fc29051523e
Francisco Souza <f@souza.cc>
2011-11-17 21:44:25 -0200

    Mapeando /media/ localmente

--- devincachu/urls.py
+++ devincachu/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import include, patterns, url
 from django.views.generic import list
 
@@ -13,3 +14,8 @@ urlpatterns = patterns('',
     url(r'^palestrantes$', pviews.PalestrantesView.as_view(), name='palestrantes'),
     url(r'^$', list.ListView.as_view(template_name='index.html', queryset=models.Destaque.objects.select_related().order_by('-data')[:10], context_object_name='destaques'), name='index'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
