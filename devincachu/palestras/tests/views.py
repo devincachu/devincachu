c3dc09091d98c8c17be0dbe51702677534e8ae89
Francisco Souza <f@souza.cc>
2011-11-17 20:11:00 -0200

    Mapeando URL /palestrantes para lista de palestrantes

--- devincachu/palestras/tests/views.py
+++ devincachu/palestras/tests/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from django.core import urlresolvers as u
 from django.views.generic import list
 
 from palestras import models, views
@@ -19,3 +20,8 @@ class PalestrantesViewTestCase(unittest.TestCase):
 
     def test_context_object_name_deve_ser_palestrantes(self):
         self.assertEquals("palestrantes", views.PalestrantesView.context_object_name)
+
+    def test_deve_estar_mapeado_para_url_palestrantes(self):
+        f = views.PalestrantesView.as_view()
+        resolve = u.resolve("/palestrantes")
+        self.assertEquals(f.func_code, resolve.func.func_code)
--- devincachu/urls.py
+++ devincachu/urls.py
@@ -5,9 +5,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 from destaques import models
+from palestras import views as pviews
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^palestrantes$', pviews.PalestrantesView.as_view(), name='palestrantes'),
     url(r'^$', list.ListView.as_view(template_name='index.html', queryset=models.Destaque.objects.select_related().order_by('-data')[:10], context_object_name='destaques'), name='index'),
 )
