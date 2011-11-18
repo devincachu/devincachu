c22b619eb1a3c944f35a37f92d2326255f738566
Francisco Souza <f@souza.cc>
2011-11-18 19:11:07 -0200

    Tratando classe active no menu via javascript

---
+++ devincachu/static_files/js/init.menu.js
@@ -0,0 +1,17 @@
+(function(global, document) {
+    if (global._ === undefined) {
+        global._ = {};
+    }
+
+    global._.definirAtivo = function() {
+        var i, path = global.location.href.replace(/\/$/, '').replace(/^http:\/\/.*\//, '');
+        var re = new RegExp(path + '$');
+        var itens = document.querySelectorAll('.nav > li > a');
+        for (i = 0; i < itens.length; i++) {
+            if (re.test(itens[i].href)) {
+                itens[i].parentNode.classList.add('active');
+                break;
+            }
+        }
+    };
+})(window, window.document);
