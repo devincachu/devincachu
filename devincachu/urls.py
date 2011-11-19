721ef7a6e8062b6ef4581b26a9b0c9e41fff7e67
Francisco Souza <f@souza.cc>
2011-11-19 06:17:03 -0200

    Colocando / na url de palestrante

--- devincachu/urls.py
+++ devincachu/urls.py
@@ -12,7 +12,7 @@ connect()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^palestrantes$', pviews.PalestrantesView.as_view(), name='palestrantes'),
+    url(r'^palestrantes/$', pviews.PalestrantesView.as_view(), name='palestrantes'),
     url(r'^$', list.ListView.as_view(template_name='index.html', queryset=models.Destaque.objects.select_related().order_by('-data')[:10], context_object_name='destaques'), name='index'),
 )
 
