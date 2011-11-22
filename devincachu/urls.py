e0c686fccaebd94ca83b63e89e01510ce00299f6
Francisco Souza <f@souza.cc>
2011-11-22 21:57:47 -0200

    Melhorando queryset para não trazer chamadas

--- devincachu/urls.py
+++ devincachu/urls.py
@@ -13,7 +13,7 @@ connect()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^palestrantes/$', pviews.PalestrantesView.as_view(), name='palestrantes'),
-    url(r'^$', list.ListView.as_view(template_name='index.html', queryset=models.Destaque.objects.select_related().order_by('-data')[:10], context_object_name='destaques'), name='index'),
+    url(r'^$', list.ListView.as_view(template_name='index.html', queryset=models.Destaque.objects.select_related().filter(chamada__isnull=True).order_by('-data')[:10], context_object_name='destaques'), name='index'),
 )
 
 if settings.DEBUG:
