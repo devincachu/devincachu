dbce1dabb29d767ce8562470084ef25bf6501e29
Francisco Souza <f@souza.cc>
2012-01-25 14:09:15 -0200

    Mudando estrutura dos testes

--- devincachu/palestras/tests/__init__.py
+++ devincachu/palestras/tests/__init__.py
@@ -3,4 +3,4 @@ from palestras.tests.admin_palestrante import *
 from palestras.tests.forms import *
 from palestras.tests.model_palestra import *
 from palestras.tests.model_palestrante import *
-from palestras.tests.views import *
+from palestras.tests.views_palestrante import *
