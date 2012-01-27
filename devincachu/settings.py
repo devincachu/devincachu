7db0efb0797af55df18701ec3ae77f240340a603
Francisco Souza <f@souza.cc>
2012-01-27 00:24:26 -0200

    Corrigindo context processors

--- devincachu/settings.py
+++ devincachu/settings.py
@@ -61,10 +61,12 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
     "core.processors.get_base_url",
 )
 
