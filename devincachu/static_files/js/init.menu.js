c5a5eda058ef2f223b746e25600265c4985ba11c
Francisco Souza <f@souza.cc>
2012-01-30 20:42:56 -0200

    Ajustando js de definição de item ativo no menu

--- devincachu/static_files/js/init.menu.js
+++ devincachu/static_files/js/init.menu.js
@@ -5,7 +5,7 @@
 
     global._.definirAtivo = function() {
         var i, path = global.location.href.replace(/\/$/, '').replace(/^http:\/\/.*\//, '');
-        var re = new RegExp(path + '$');
+        var re = new RegExp(path + '/$');
         var itens = document.querySelectorAll('.nav > li > a');
         for (i = 0; i < itens.length; i++) {
             if (re.test(itens[i].href)) {
@@ -14,4 +14,6 @@
             }
         }
     };
+
+    document.getElementsByTagName("body")[0].onload = global._.definirAtivo();
 })(window, window.document);
