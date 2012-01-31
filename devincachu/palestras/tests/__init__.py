77767fdf444b240c76e9eab9b4410cc343b29ac7
Francisco Souza <f@souza.cc>
2012-01-31 20:05:40 -0200

    Corrigindo import do teste

--- devincachu/palestras/tests/__init__.py
+++ devincachu/palestras/tests/__init__.py
@@ -4,5 +4,5 @@ from palestras.tests.forms import *
 from palestras.tests.model_palestra import *
 from palestras.tests.model_palestrante import *
 from palestras.tests.view_palestra import *
-from palestras.tests.view_palestrante import *
+from palestras.tests.view_palestrantes import *
 from palestras.tests.view_programacao import *
